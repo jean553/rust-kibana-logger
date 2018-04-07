3f0327e44becdc5ce831ed3cc59606588d6d8666
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-07 12:56:30 +0200

    integrate first implementation of the log_info

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,48 @@
-mod lib {
+extern crate syslog;
+
+use syslog::{
+    Facility,
+    Logger,
+};
+
+pub trait KibanaLogger {
+
+    fn new() -> Logger;
+
+    fn log_info(&self);
+}
+
+impl KibanaLogger for Logger {
+
+    /// Creates a new syslog logger object.
+    ///
+    /// Returns:
+    ///
+    /// syslog logger
+    fn new() -> Logger {
+        *syslog::unix(Facility::LOG_LOCAL7).unwrap()
+    }
+
+    /// Logs a message into syslog with the `info` level.
+    ///
+    /// TODO: should take a JSON object as parameter
+    fn log_info(&self) {
+        let _ = self.info("a log message");
+    }
 }
 
 #[cfg(test)]
 mod tests {
+
+    /* TODO: we should use only one external object */
+
+    use KibanaLogger;
+    use syslog::Logger;
+
+    #[test]
+    fn test_info() {
+
+        let logger = Logger::new();
+        logger.log_info();
+    }
 }
